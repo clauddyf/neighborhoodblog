4a3dc507208bdba8197a4ae4ed64c0f1b3748b86
Claudius <claudius.solomon@gmail.com>
2020-11-07 20:55:57 -0800

    created the city model

--- neighborhoodblog/app/models/city.rb
+++ neighborhoodblog/app/models/city.rb
@@ -1,2 +1,7 @@
 class City < ApplicationRecord
+    validates :city_name, presence: true
+
+    has_many :hbns, class_name: :Hbn, foreign_key: :city_id 
+
+    has_many :artists, class_name: :Artist, foreign_key: :city_id
 end
