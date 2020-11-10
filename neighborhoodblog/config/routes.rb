3748ddfcf242c8c4f8cbed67959823eeb003cf47
Claudius <claudius.solomon@gmail.com>
2020-11-09 20:45:20 -0800

    created the api routes

--- neighborhoodblog/config/routes.rb
+++ neighborhoodblog/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do 
+    
+    resources :cities, only: [:show,:index]
+    resources :hbns, only: [:show, :index]
+    resources :artist, only: [:show,:index]
+    resource :session, only: [:create, :destroy, :show]
+    resources :users, only: [:create, :update, :show, :index]
+  end
 end
