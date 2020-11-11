08503ec93ca5ae1c0b6f5d9fc5edc1eddc9dbb24
Claudius <claudius.solomon@gmail.com>
2020-11-11 14:59:52 -0800

    working on dev dependencies

---
+++ neighborhoodblog/webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require("webpack");
+const path = require('path');
+module.exports = {
+    context: __dirname,
+    entry: './frontend/neighborhood.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    devtool: 'source-map'
+};
